d3e7b2f830610411d19bbb92fed52381ad833c20
marcela05 <marcelachito@gmail.com>
2016-05-24 14:30:39 -0500

    modificando las funciones

--- imports/api/collections.js
+++ imports/api/collections.js
@@ -5,16 +5,21 @@ import { check } from 'meteor/check';
 
 export const Users = new Mongo.Collection('users');
 export const Libros = new Mongo.Collection('libros');
-
 Meteor.methods({
-   'Libros.insert'(nombre, ano, isbn, autor) {
-   
-    Users.insert({
-    nombre,
-    ano,
-    isbn,
-    autor,
+   'libros.insert'(nombre, ano, autor, isbn, estado) {
+    Libros.insert({
+    nombre:nombre,
+    ano:ano,    
+    autor:autor,
+    isbn:isbn,
+    estado:estado
     });
   },
 });
 
+if (Meteor.isServer) {
+  // This code only runs on the server
+  Meteor.publish('users', function tasksPublication() {
+    return Users.find();
+  });
+}
